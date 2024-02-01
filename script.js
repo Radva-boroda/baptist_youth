07d114fcc5c8258d3f2ddc86333f760913cc17f3
Nazarcody <booooooroda@icloud.com>
2024-02-01 13:07:23 +0200

    Add fonts

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         prevEl: '.btn_back',
       },
     });
-  
+  //клік // свай дороб 
     document.querySelector('.btn_back').addEventListener('click', function () {
       mySwiper.slidePrev(1);
     });
